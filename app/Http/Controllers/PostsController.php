e63866451bf6cdeb351ee5c7acac7b1fe95783fd
natuhvnh <natuhvnh@gmail.com>
2019-12-08 21:52:34 +0700

    Init backend

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Post;
+
+class PostsController {
+  public function show($post) {
+    // $posts = [
+    //     'my-first-post' => 'Hello, this is my first post',
+    //     'my-second-post' => 'Hello, this is my second post'
+    // ];
+    $slug = Post::where('slug', $post)->firstOrFail();  //firstOrFail == if (! $slug) {abort(404)}
+    // dd($slug);
+    return $slug->body; 
+  }
+}
+
+// class PostsController {
+//   public function show($post) {
+//     // $posts = [
+//     //     'my-first-post' => 'Hello, this is my first post',
+//     //     'my-second-post' => 'Hello, this is my second post'
+//     // ];
+//     $slug = DB::table('posts')->where('slug', $post)->first();
+//     if (! $slug) {
+//       abort(404);
+//     }
+//     // dd($slug);
+//     return $slug->body; 
+//   }
+// }
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// User get request to home page => response loding view welcome in resources/views
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/posts/{post}', function ($post) {
+//     $posts = [
+//         'my-first-post' => 'Hello, this is my first post',
+//         'my-second-post' => 'Hello, this is my second post'
+//     ];
+//     return $posts[$post];
+// }); 
+
+Route::get('/posts/{post}', 'PostsController@show');
